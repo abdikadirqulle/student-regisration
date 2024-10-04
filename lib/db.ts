23ae558110764296db2f2dc814ee2c9add17b259
abdikadir qulle <wlaaldaadaa33@gmail.com>
2024-10-04 17:27:30 +0300

    some prettier writte

--- lib/db.ts
+++ lib/db.ts
@@ -1,21 +1,21 @@
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient } from "@prisma/client";
 
 declare global {
   // Prevent variable declaration conflicts in the global scope during hot-reloading in development
   // eslint-disable-next-line no-var
-  var prisma: PrismaClient | undefined
+  var prisma: PrismaClient | undefined;
 }
 
-let db: PrismaClient
+let db: PrismaClient;
 
 // Ensure PrismaClient is instantiated only once in development and not recreated on every module reload
 if (process.env.NODE_ENV === "production") {
-  db = new PrismaClient()
+  db = new PrismaClient();
 } else {
   if (!global.prisma) {
-    global.prisma = new PrismaClient()
+    global.prisma = new PrismaClient();
   }
-  db = global.prisma
+  db = global.prisma;
 }
 
-export default db
+export default db;
